a779a862a6ec3f11fa92c51b870e3bfbd20ee7f9
Tam <Tam@users.noreply.github.com>
2024-04-05 13:36:53 +0100

    add dependencies

--- api/test/index.ts
+++ api/test/index.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env deno run --allow-net --allow-env --allow-read --allow-sys
 
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import pg from 'npm:pg@8';
 import example from '../../src/example.ts';
 
 export const handler = async (req: Request) => {
-	console.log(req.url);
+	console.log(req.url, createClient, pg);
 	return await example();
 };
 
